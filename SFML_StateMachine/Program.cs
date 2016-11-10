9985a2328fe21cf9d16c532aa3cf860f5c8632e1
RazielAntemos <RazielAntemos@users.noreply.github.com>
2016-11-10 13:57:00 +0100

    Add files via upload

---
+++ SFML_StateMachine/Program.cs
@@ -0,0 +1,28 @@
+﻿using GameEngine; //namespace with all engine related shit. Check that every class for player/item/etc runs in this namespace.
+
+namespace StateMachine
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var game = new GameObject("State Machine");
+
+            // build the startup menu scene
+
+            MainScene _mainScene = new MainScene(game);
+            _mainScene.Name = "main";
+
+            game.SceneManager.AddScene(_mainScene);
+
+            StartScene _startScene = new StartScene(game);
+            _startScene.Name = "start";
+            game.SceneManager.AddScene(_startScene);
+
+            //create here as many screens as we need but dont forget to make a class for each
+
+            // Start the game
+            game.SceneManager.StartScene("start"); //it is one of the above names, scene is called by its name!
+        }
+    }
+}
---
+++ SFML_StateMachine/Scene.cs
@@ -0,0 +1,128 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+
+namespace GameEngine
+{
+    public class Scene : IDisposable
+    {
+        public string Name;
+
+        private DateTime currentTime = System.DateTime.Now;
+
+        private DateTime targetTime = System.DateTime.Now;
+
+        private bool pause = false;
+
+        public bool IsPaused
+        {
+            get { return pause; }
+        }
+
+        protected GameObject _gameObject;
+
+        public Color BackgroundColor = Color.Black; //default scene color
+
+        public Scene(GameObject gameObject)
+        {
+            _gameObject = gameObject;
+        }
+
+        public virtual void Initialize()
+        {
+            // initialize the game
+        }
+
+        public virtual void Reset()
+        {
+            // when the scene resets do smth
+        }
+
+        public void Run()
+        {
+            // kinda main loop
+
+            while (_gameObject.Window.IsOpen)
+            {
+                currentTime = System.DateTime.Now;
+
+                _gameObject.Window.Clear(this.BackgroundColor);
+
+                if (!pause)
+                {
+                    this.Update();
+
+                    this.Draw();
+
+                    _gameObject.Window.Display();
+                }
+                else
+                {
+                    if (currentTime >= targetTime)
+                    {
+                        pause = false;
+                    }
+                    else
+                        this.OnPause();
+                }
+
+                _gameObject.Window.DispatchEvents();
+            }
+        }
+
+        public virtual void HandleKeyPress(KeyEventArgs e)
+        {
+            //  input handler for the scene
+        }
+
+        public virtual void HandleKeyReleased(KeyEventArgs e)
+        {
+            //  input handler for the scene
+        }
+
+        public virtual void Update()
+        {
+        }
+
+        public virtual void Draw()
+        {
+        }
+
+        public void Pause(int milliseconds)
+        {
+            targetTime = currentTime.AddMilliseconds(milliseconds);
+            pause = true;
+        }
+
+        public void Resume()
+        {
+            pause = false;
+        }
+
+        public virtual void OnPause()
+        {
+            // called each frame during the pause
+        }
+
+        public virtual void Exit()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+               //do smth
+            }
+        }
+    }
+}
+
+
