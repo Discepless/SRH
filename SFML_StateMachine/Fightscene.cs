032fae76382cd22192d50d191c0888d585a4a109
pete <pete@ZOAY>
2016-11-27 20:52:32 +0100

    Sword and Arrow Adjustments

--- SFML_StateMachine/Fightscene.cs
+++ SFML_StateMachine/Fightscene.cs
@@ -13,29 +13,33 @@ using System.Threading.Tasks;
 namespace StateMachine
 {
     public class Fightscene : Scene
-    {
-        //Character
+    {//Objekte und Variablen werden erstellt:
+
+        //Avatar(Character)
         private Texture character_img;
         private Sprite character_sprite;
+
         //Enemy
         private Texture enemy_img;
         private Sprite enemy_sprite;
+
         //Buttons
         private Texture attack_button_img;
         private Texture defend_button_img;
         private Sprite attack_button_sprite;
         private Sprite defend_button_sprite;
+
         //Text
         private Text Nahkampf_Text;
         private Text Fernkampf_Text;
         private Text HP_Text;
         private Text EnemyHP_Text;
+
         //Healthbar
         private Texture healthbar_img;
         private Sprite healthbar_sprite;
 
         private RectangleShape healthbar_rectangle;
-
 
         //Enemy Healthbar
         private Texture enemy_healthbar;
@@ -52,42 +56,51 @@ namespace StateMachine
         private Sprite sword_sprite;
 
         //Timer
-        Clock clock;
+        Clock clock_EnemiesTurn;
+        Clock clock_SwordSlideIn;
 
-
+        //Character & Enemy SlideIn Speed
         float Speed = 1;
 
+        //Character Stats
         int HP = 100;
-        int EnemyHP = 70;
         int healthLeft = 100;
-        int enemyHealthLeft = 70;
         int Attack_Nahkampf = 20;
-        int EnemyAttack = 25;
         int Attack_Fernkampf = 10;
 
-        bool Charcters_Turn = true;
+        //Enemy Stats
+        int EnemyHP = 70;
+        int enemyHealthLeft = 70;
+        int EnemyAttack = 25;
+
+        //Handles whos turn it is
+        bool Characters_Turn = true;
         bool Enemies_Turn = false;
 
+        //Handles  if character got hit
         bool HealthDown = false;
         bool EnemiesHealthDown = false;
 
+        //Handles which Attack-Typ is used
         bool Nahkampf = false;
         bool Fernkampf = false;
 
+        //Handles start of Attack-Animation
         bool Arrow_Start = false;
         bool Sword_Start = false;
+        bool Sword_Time = false;
 
         public Fightscene(GameObject gameObject) : base(gameObject)
         {
-            this.BackgroundColor = Color.White;
+            BackgroundColor = Color.White;
 
         }
 
         public override void Initialize()
-        {
+        {//Objekte werden initialisiert und zugewiesen
+
             //Character
             character_img = new Texture("character_fight.png");
-
             character_sprite = new Sprite(character_img);
 
             character_sprite.Position = new Vector2f(650, 400);
@@ -95,7 +108,6 @@ namespace StateMachine
 
             //Enemy
             enemy_img = new Texture("Enemy.png");
-
             enemy_sprite = new Sprite(enemy_img);
 
             enemy_sprite.Position = new Vector2f(0, 70);
@@ -103,7 +115,6 @@ namespace StateMachine
 
             //Arrow
             arrow_img = new Texture("arrow.jpg");
-
             arrow_sprite = new Sprite(arrow_img);
 
             arrow_sprite.Position = new Vector2f(220, 200);
@@ -113,7 +124,7 @@ namespace StateMachine
             sword_img = new Texture("sword.png");
             sword_sprite = new Sprite(sword_img);
 
-            sword_sprite.Position = new Vector2f(220, 200);
+            sword_sprite.Position = new Vector2f(470, 100);
             sword_sprite.Scale = new Vector2f(.07f, .07f);
 
             //Buttons
@@ -137,36 +148,24 @@ namespace StateMachine
             Font arial = new Font(@"Resources\arial.ttf");
                   //Text Nahkampf_Button
             Nahkampf_Text = new Text("", arial);
-
             Nahkampf_Text.Position = new Vector2f(0, 0);
-
             Nahkampf_Text.CharacterSize = 25;
-
                  //Text Fernkampf_Button
             Fernkampf_Text = new Text("", arial);
-
             Fernkampf_Text.Position = new Vector2f(0, 0);
-
             Fernkampf_Text.CharacterSize = 25;
 
-                //HP
+            //HP
                   //HP Character
             Font system = new Font(@"Resources\Capture_it.ttf");
-
             HP_Text = new Text("", system);
-
             HP_Text.Position = new Vector2f(0, 0);
-
             HP_Text.CharacterSize = 20;
-
             HP_Text.Color = Color.Red;
                  //HP Enemy
             EnemyHP_Text = new Text("", system);
-
             EnemyHP_Text.Position = new Vector2f(0, 0);
-
             EnemyHP_Text.CharacterSize = 10;
-
             EnemyHP_Text.Color = Color.Black;
 
             //Healthbar_img
@@ -175,9 +174,7 @@ namespace StateMachine
 
             healthbar_sprite.Position = new Vector2f(10, 350);
             healthbar_sprite.Scale = new Vector2f(.5f, .5f);
-
-            healthbar_rectangle = new RectangleShape();
-            // healthbar_rectangle.TextureRect = new IntRect(0, 0, healthbar_sprite.TextureRect.Width * health / 3, healthbar_sprite.TextureRect.Height);
+            healthbar_rectangle = new RectangleShape();          
 
             //Healthbar Enemies
             enemy_healthbar = new Texture("healthbar.png");
@@ -185,44 +182,49 @@ namespace StateMachine
 
             enemy_healthbar_sprite.Position = new Vector2f(565, 20);
             enemy_healthbar_sprite.Scale = new Vector2f(.5f, .5f);
-
             enemy_healthbar_rectangle = new RectangleShape();
 
             //Timer
-            clock = new Clock();
-
+            clock_EnemiesTurn = new Clock();
+            clock_SwordSlideIn = new Clock();
 
             base.Initialize();
         }
 
         public override void HandleKeyPress(KeyEventArgs e)
         {
-            if (e.Code == Keyboard.Key.A && Charcters_Turn && healthLeft > 0)
+            //Nahkampf
+            if (e.Code == Keyboard.Key.A && Characters_Turn && healthLeft > 0)
             {
                 EnemiesHealthDown = true;
                 Attack_SlideInMove();
-                Charcters_Turn = false;
+                Characters_Turn = false;
                 Enemies_Turn = true;
                 Nahkampf = true;
                 Sword_Start = true;
-                clock.Restart();
+                clock_EnemiesTurn.Restart();
+                clock_SwordSlideIn.Restart();
             }
-            if (e.Code == Keyboard.Key.D && Charcters_Turn && healthLeft > 0)
+
+            //Fernkampf
+            if (e.Code == Keyboard.Key.D && Characters_Turn && healthLeft > 0)
             {
-                // HealthHandler();
                 EnemiesHealthDown = true;
                 Attack_SlideInMove();
                 Enemies_Turn = true;
-                Charcters_Turn = false;
+                Characters_Turn = false;
                 Fernkampf = true;
                 Arrow_Start = true;
-                clock.Restart();
+                clock_EnemiesTurn.Restart();
             }
         }
 
         public override void Update()
-        {
-            Console.WriteLine(sword_sprite.Rotation);
+        {//Fightscene Logic
+
+            Console.WriteLine(clock_SwordSlideIn.ElapsedTime.AsSeconds());
+            Console.WriteLine(Sword_Start);
+            Console.WriteLine(Sword_Time);
             //Character Slide In
             if (SlideInMove_character())
                 character_sprite.Position -= new Vector2f(15, 0) * Speed;
@@ -244,7 +246,7 @@ namespace StateMachine
                 arrow_sprite.Position += new Vector2f(10, -10);
             else
             {
-                arrow_sprite.Position = new Vector2f(220, 200);
+                arrow_sprite.Position = new Vector2f(250, 300);
                 Arrow_Start = false;
             }
 
@@ -260,13 +262,11 @@ namespace StateMachine
             //Text
             string t1 = "Nahkampf [A] [" + Attack_Nahkampf + " SP]";
             Nahkampf_Text.DisplayedString = t1;
-            Nahkampf_Text.Position = new Vector2f(/*button_sprite.Position.X, button_sprite.Position.Y*/378, 420);
-            Nahkampf_Text.Draw(_gameObject.Window, RenderStates.Default);
+            Nahkampf_Text.Position = new Vector2f(378, 420);
 
             string t2 = "Fernkampf [D] [" + Attack_Fernkampf + " SP]";
             Fernkampf_Text.DisplayedString = t2;
             Fernkampf_Text.Position = new Vector2f(378, 510);
-            //Defend_Text.Draw(_gameObject.Window, RenderStates.Default);
 
             string t3 = "HP " + healthLeft + " / " + HP;
             HP_Text.DisplayedString = t3;
@@ -277,13 +277,13 @@ namespace StateMachine
             EnemyHP_Text.Position = new Vector2f(627, 24);
 
             //Health
-            //Character
+                 //Character
             if (HealthDown)
             {
                 CharacterHealthHandler();
                 HealthDown = false;
             }
-            //Enemies
+                 //Enemies
             if (EnemiesHealthDown)
             {
                 EnemyHealthHandler();
@@ -291,16 +291,20 @@ namespace StateMachine
             }
 
             //Timer (Enemies Turn)
-            if (clock.ElapsedTime.AsSeconds() >= 2 && Enemies_Turn && enemyHealthLeft > 0)
+            if (clock_EnemiesTurn.ElapsedTime.AsSeconds() >= 3 && Enemies_Turn && enemyHealthLeft > 0)
             {
                 HealthDown = true;
                 Enemy_Attack_SlideInMove();
                 Enemies_Turn = false;
-                Charcters_Turn = true;
+                Characters_Turn = true;
             }
-
-
-
+            //Timer (SwordSlideIn)
+            if (clock_SwordSlideIn.ElapsedTime.AsSeconds() <= 1)
+            {
+                Sword_Time = true;
+            }
+            else
+                Sword_Time = false;
             //Draws
             _gameObject.Window.Draw(character_sprite);
             _gameObject.Window.Draw(enemy_sprite);
@@ -324,10 +328,14 @@ namespace StateMachine
             if (Arrow_Start)
                 _gameObject.Window.Draw(arrow_sprite);
 
-            if (Sword_Start)
+            if (/*Sword_Start*/Sword_Time)
                 _gameObject.Window.Draw(sword_sprite);
 
         }
+
+        //Erstellte Methoden
+
+            //Handles objects when to move
 
         public bool SlideInMove_character()
         {
@@ -352,7 +360,7 @@ namespace StateMachine
 
         public bool Arrow_move()
         {
-            if (arrow_sprite.Position.X >= 220 && arrow_sprite.Position.X <= 300 && Arrow_Start)
+            if (arrow_sprite.Position.X >= 250 && arrow_sprite.Position.X <= 450 && Arrow_Start)
                 return true;
 
             return false;
@@ -372,24 +380,19 @@ namespace StateMachine
             character_sprite.Position += new Vector2f(150, 0);
         }
 
-        //public void Arrow_SlideInMove()
-        //{
-        //    arrow_sprite.Position += new Vector2f(120, 120);
-        //}
 
         public void Enemy_Attack_SlideInMove()
         {
             enemy_sprite.Position -= new Vector2f(150, 0);
         }
 
-
+            //Handles Health
 
         public void DecreaseHealth()
         {
             healthLeft -= EnemyAttack;
-            // HP -= Defense;
-
         }
+
         public void DecreaseEnemyHealth()
         {
             if (Nahkampf)
@@ -402,20 +405,12 @@ namespace StateMachine
                 enemyHealthLeft -= Attack_Fernkampf;
                 Fernkampf = false;
             }
-            //enemyhealth -= Attack;
-            //EnemyHP -= Attack;
         }
+
         public void CharacterHealthHandler()
         {
-
-            // attackcooldown -= clock.ElapsedTime.AsMilliseconds() / 1000f;
-
-            // if (attackcooldown <= 5)
-            //{
             DecreaseHealth();
 
-            //}
-            // attackcooldown = 5f;
             healthbar_rectangle.TextureRect = new IntRect(0, 0, healthbar_sprite.TextureRect.Width * healthLeft / HP, healthbar_sprite.TextureRect.Height);
             healthbar_sprite.TextureRect = healthbar_rectangle.TextureRect;
         }
